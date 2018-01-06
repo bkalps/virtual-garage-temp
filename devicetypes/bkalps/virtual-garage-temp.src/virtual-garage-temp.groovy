0168bc8b245d19071528f0dfe28ae19901e0ba15
bkalps <brian.alpert@gmail.com>
2018-01-06 11:36:53 -0600

    Update virtual-garage-temp.groovy
    Removed "Test Update" comment.

--- devicetypes/bkalps/virtual-garage-temp.src/virtual-garage-temp.groovy
+++ devicetypes/bkalps/virtual-garage-temp.src/virtual-garage-temp.groovy
@@ -10,7 +10,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Modified the SmartThings Simulated Temperature Measurement code. TEST UPDATE!
+ *  Modified the SmartThings Simulated Temperature Measurement code.
  */
 metadata {
 	// Automatically generated. Make future change here.
